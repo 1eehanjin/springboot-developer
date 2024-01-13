e473df1f0ea855aed6335a0b59ec39fa88bdf7c0
1eehanjin <jdhanjin@naver.com>
2024-01-13 15:59:19 +0900

    4. 스프링 부트3와 테스트

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private  Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ src/main/java/com/example/demo/service/TestService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll();
+    }
+}
